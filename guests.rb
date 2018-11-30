82d185f1f7f562089fad229eb10e4880da8733ac
Joe McColl <joe4kool@gmail.com>
2018-11-30 17:12:35 +0000

    added customer wallet value to guest

--- guests.rb
+++ guests.rb
@@ -1,10 +1,12 @@
 class Guests
 
   attr_reader :name, :favourite_song
+  attr_accessor :customer_wallet
 
-  def initialize(name, favourite_song)
+  def initialize(name, favourite_song, customer_wallet)
     @name = name
     @favourite_song = favourite_song
+    @customer_wallet = customer_wallet
   end
 
 end
--- specs/guests_spec.rb
+++ specs/guests_spec.rb
@@ -6,11 +6,15 @@ require_relative("../guests")
 class GuestsTest < MiniTest::Test
 
   def setup
-    @guest = Guests.new("Joe", "Sonne")
+    @guest = Guests.new("Joe", "Sonne", 30)
   end
 
   def test_guest_class_exists
     assert_equal(Guests, @guest.class())
   end
+
+  # def test_money_in_wallet
+  #   assert_equal()
+  # end
 
 end
