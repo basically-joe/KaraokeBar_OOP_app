cf84b29f31267575b5221975c3789a3d31e7305a
Joe McColl <joe4kool@gmail.com>
2018-11-30 13:22:24 +0000

    tested that guests class is creating new guest object, test passing

--- specs/guests_spec.rb
+++ specs/guests_spec.rb
@@ -6,11 +6,11 @@ require_relative("../guests")
 class GuestsTest < MiniTest::Test
 
   def setup
-
+    @guest = Guests.new("Joe")
   end
 
-  def test_guest_exist
-
+  def test_guest_class_exists
+    assert_equal(Guests, @guest.class())
   end
 
 end
