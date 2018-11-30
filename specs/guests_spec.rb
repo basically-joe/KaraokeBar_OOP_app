9189c4e60a47d70dd8c10ea89f67412a8b185ea8
Joe McColl <joe4kool@gmail.com>
2018-11-30 13:17:05 +0000

    created guest class and test passing for file setup

---
+++ specs/guests_spec.rb
@@ -0,0 +1,16 @@
+require("minitest/rg")
+require("minitest/autorun")
+
+require_relative("../guests")
+
+class GuestsTest < MiniTest::Test
+
+  def setup
+
+  end
+
+  def test_guest_exist
+
+  end
+
+end
